66e7e037c8fd6698f309a3eee0f0bf8d3841a4c4
Marjankovacev <marjankovacev023@gmail.com>
2023-03-08 18:42:21 +0100

    Jedan negativan test

--- cypress/e2e/Register.cy.js
+++ cypress/e2e/Register.cy.js
@@ -7,7 +7,18 @@ describe("Successful registration", () => {
         cy.visit("/");
         cy.get('a[href="/register"]').click();
     })
-    it("Fill register imputs", () => {
+    it("empty firstname NEG ", ()=>{
+        cy.visit("/register");
+        cy.get("#last-name").type("kovacevic");
+        cy.get("#email").type("Kovacevic@gmail.com");
+        cy.get("#password").type("Kovacevic1");
+        cy.get("#password-confirmation").type("Kovacevic1");
+        cy.get('input[type="checkbox"]').click();
+        cy.get('button[type="submit"]').click();
+        cy.url().should("contain","/register");
+    })
+
+    it("Fill register imputs correct", () => {
         cy.visit("/register");
         cy.get("#first-name").type("Miroslav");
         cy.get("#last-name").type("Nenadovic");
@@ -16,7 +27,7 @@ describe("Successful registration", () => {
         cy.get("#password-confirmation").type("Nekasifra123");
         cy.get('input[type="checkbox"]').click();
         cy.get('button[type="submit"]').click();
-        
+        cy.url().should("contain","/logout");
     })
     
 })
